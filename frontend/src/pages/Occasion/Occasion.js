5ac910a1ef9bd6b6960f76e606a3c39f2993eb18
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-03 19:05:14 +0200

    frontend:adding images

--- frontend/src/pages/Occasion/Occasion.js
+++ frontend/src/pages/Occasion/Occasion.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./styleOccasion.css";
 import Hexagon from "../../common/base/hexagon/Hexagon";
 import Outdoor from "../../common/images/Outdoor-Adventure.jpeg";
-import swim from "../../common/images/swim.jpeg";
-// import swim from "../../common/images/swim.jpeg";
-// import swim from "../../common/images/swim.jpeg";
-// import swim from "../../common/images/swim.jpeg";
-// import swim from "../../common/images/swim.jpeg";
-// import swim from "../../common/images/swim.jpeg";
-// import swim from "../../common/images/swim.jpeg";
+import beach from "../../common/images/Beach-Vacation.jpeg";
+import Athletic from "../../common/images/Athletic.png";
+import casual from "../../common/images/Casual-Outing.jpg";
+import meeting from "../../common/images/Business-Meeting.jpeg";
+import sleepover from "../../common/images/Sleepover.jpeg";
+import Traditional from "../../common/images/Traditional-Celebration.jpeg";
+import wedding from "../../common/images/wedding.jpeg";
 const Occasion = () => {
   return (
     <div>
@@ -16,19 +16,19 @@ const Occasion = () => {
       <section className="hexagons-temp">
         <div className="hexa3 flex center">
           <Hexagon bgImage={Outdoor} hexaText="OutDoor Activity" />
-          <Hexagon bgImage={swim} hexaText="Beachparty" />
-          <Hexagon bgImage={swim} hexaText="Beachparty" />
+          <Hexagon bgImage={beach} hexaText="Beach Vacation" />
+          <Hexagon bgImage={Athletic} hexaText="Athletic Activity" />
         </div>
         <div className="hexa4 flex center">
-          <Hexagon bgImage={swim} />
-          <Hexagon bgImage={swim} />
-          <Hexagon bgImage={swim} />
-          <Hexagon bgImage={swim} />
+          <Hexagon bgImage={meeting} hexaText="OutDoor Activity" />
+          <Hexagon bgImage={sleepover} hexaText="Beach Vacation" />
+          <Hexagon bgImage={Traditional} hexaText="Athletic Activity" />
+          <Hexagon bgImage={wedding} hexaText="OutDoor Activity" />
         </div>
         <div className="hexa3 flex center">
-          <Hexagon bgImage={swim} />
-          <Hexagon bgImage={swim} />
-          <Hexagon bgImage={swim} />
+          <Hexagon bgImage={casual} hexaText="OutDoor Activity" />
+          <Hexagon bgImage={beach} hexaText="Beach Vacation" />
+          <Hexagon bgImage={Athletic} hexaText="Athletic Activity" />
         </div>
       </section>
     </div>
