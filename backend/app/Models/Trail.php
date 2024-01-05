a5e9709ce9f4d2951e1a969613f1f9e4ef4402ac
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-05 14:06:13 +0200

    backend:implementing for trails model, the fillable and the relationship between it and other table

--- backend/app/Models/Trail.php
+++ backend/app/Models/Trail.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Trail extends Model
 {
-    use HasFactory;
+    protected $fillable = ['choosen', 'user_id', 'clothes_id'];
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function clothes(): BelongsTo
+    {
+        return $this->belongsTo(Clothes::class);
+    }
 }
