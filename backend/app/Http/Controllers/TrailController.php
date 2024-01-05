1aa9f5b9d041f6dd5143975005fcfdc92b91e972
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-05 14:40:51 +0200

    backend:creating updateTrail so when the user choose the costume it change choosen arrow to 1

--- backend/app/Http/Controllers/TrailController.php
+++ backend/app/Http/Controllers/TrailController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class TrailController extends Controller
 {
-    //
+    function updateTrail(Request $request){
+        try {
+            if (Auth::check()) {
+                $user = Auth::user();
+                $trail=Trail::get($user->user_id);
+                $request->validate([
+                    'choosen' => 'required',
+                ]);
+            }
+            } catch (QueryException $e) {
+              
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'Database error: ' . $e->getMessage(),
+                ]);
+
+    }
 }
