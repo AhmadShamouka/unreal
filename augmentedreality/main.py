527907c4bfccdcec0dc9f3e5f955b69abaca6193
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-07 12:00:25 +0200

    augmentedreality:adding the file to get the t-shirt

--- augmentedreality/main.py
+++ augmentedreality/main.py
@@ -1,8 +1,9 @@
 import os
 import cvzone
 import cv2
-shirtFolderPath = "Resources"
-listShirts = os.listdir(shirtFolderPath)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+shirtFolderPath = os.path.join(script_dir, 'Resources', 'Shirts')
 
 from cvzone.PoseModule import PoseDetector
 cap = cv2.VideoCapture(0)
+print("hello")
