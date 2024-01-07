444b08f08489f529aec6817a3bd727440b0b7a10
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-07 12:04:21 +0200

    augmentedreality:adding while loop and inside it cap.read() to keep reading from the webcam

--- augmentedreality/main.py
+++ augmentedreality/main.py
@@ -3,10 +3,9 @@ import cvzone
 import cv2
 script_dir = os.path.dirname(os.path.abspath(__file__))
 shirtFolderPath = os.path.join(script_dir, 'Resources', 'Shirts')
-
+detector = PoseDetector()
 from cvzone.PoseModule import PoseDetector
 cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
     img = detector.findPose(img)
-    lmList, bboxInfo = detector.findPosition(img, draw=False, bboxWithHands=False)
