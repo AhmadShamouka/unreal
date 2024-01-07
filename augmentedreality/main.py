18c9a49f0cd6b95f6b09c40cf2cf09352b9f5134
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-07 11:54:38 +0200

    augmentedreality:Renaming the folder

--- augmentedreality/main.py
+++ augmentedreality/main.py
@@ -1,6 +1,8 @@
 import os
 import cvzone
 import cv2
+shirtFolderPath = "Resources"
+listShirts = os.listdir(shirtFolderPath)
 
 from cvzone.PoseModule import PoseDetector
 cap = cv2.VideoCapture(0)
