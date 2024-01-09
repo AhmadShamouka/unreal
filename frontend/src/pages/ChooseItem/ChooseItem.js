31cff2571d26288e31144fe409633eb7ab0e4ed9
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-09 08:52:11 +0200

    frontend:adding a title

--- frontend/src/pages/ChooseItem/ChooseItem.js
+++ frontend/src/pages/ChooseItem/ChooseItem.js
@@ -8,37 +8,41 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import wedding from "../../common/images/dress2.jpeg";
-import swim from "../../common/images/swim.jpeg";
+import sp from "../../common/images/sp.webp";
 import "./styleChooseItem.css";
 
 const ChooseItem = () => {
   return (
     <div>
       <UserNavbar />
-      <div className="choose-item-container">
-        <Swiper
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={0}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-        >
-          <SwiperSlide>
-            <img src={wedding} alt="Wedding Image" className="swiper-image" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={swim} alt="Swim Image" className="swiper-image" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={wedding} alt="Wedding Image" className="swiper-image" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={swim} alt="Swim Image" className="swiper-image" />
-          </SwiperSlide>
-        </Swiper>
+      <div className="choose-container flex center">
+        <div className="choose-label">
+          <h1>Choose Wisley</h1>
+        </div>
+        <div className="choose-items">
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={0}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            <SwiperSlide>
+              <img src={wedding} alt="Wedding" className="swiper-image" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={sp} alt="Swim" className="swiper-image" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={wedding} alt="Wedding" className="swiper-image" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={sp} alt="Swim" className="swiper-image" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
       <Footer />
     </div>
