221e38c95d349b0b31f518a61c2b64eafddff3e7
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-09 09:38:39 +0200

    frontend:Creating multiple cards

--- frontend/src/pages/Brand/Brand.js
+++ frontend/src/pages/Brand/Brand.js
@@ -6,10 +6,32 @@ const Brand = () => {
   return (
     <div>
       <UserNavbar />
-      <div>
+      <div className="brand-label flex center">
         <h1>Choose Your Brand</h1>
       </div>
-      <div className="card-brand-container">
+      <div className="card-brand-container flex center">
+        <div className="card-brand">
+          <img
+            src="https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI0OTY1ODM&ixlib=rb-1.2.1&q=80"
+            alt="balloon with an emoji face"
+            className="card-img-brand"
+          />
+          <span className="card-footer-brand">
+            <span>Awesome speedy card</span>
+            <span>2 minutes!</span>
+          </span>
+        </div>
+        <div className="card-brand">
+          <img
+            src="https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI0OTY1ODM&ixlib=rb-1.2.1&q=80"
+            alt="balloon with an emoji face"
+            className="card-img-brand"
+          />
+          <span className="card-footer-brand">
+            <span>Awesome speedy card</span>
+            <span>2 minutes!</span>
+          </span>
+        </div>
         <div className="card-brand">
           <img
             src="https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI0OTY1ODM&ixlib=rb-1.2.1&q=80"
