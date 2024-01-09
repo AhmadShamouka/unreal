e7407190df2fafcc1be5bce3427ba32608dadfb5
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-09 09:25:06 +0200

    frontend:Creating a new function Brand inside Brand.js

---
+++ frontend/src/pages/Brand/Brand.js
@@ -0,0 +1,16 @@
+import React from "react";
+import UserNavbar from "../../components/UserNavbar/UserNavbar";
+
+const Brand = () => {
+  return (
+    <div>
+      <UserNavbar />
+      <div>
+        <h1>Choose Your Brand</h1>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Brand;
