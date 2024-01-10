77d1bdf21ee5104f27abbf33ded4d0e8c76dfee7
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-10 20:57:10 +0200

    backend:adding an if condition to check if user is signed in

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -4,20 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('admin:api', ['except' => ['login', 'register']]);
+        $this->middleware('admin');
     }
 
     public function getUser()
     {
+    if(Auth::Check()){
         $getall = User::all();
         return response()->json([
             'status' => 'success',
             'response' => $getall,
         ]);
     }
+}
 }
