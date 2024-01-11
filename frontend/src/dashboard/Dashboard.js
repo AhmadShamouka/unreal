9b20dd6d4c8ad6f5d68ba4d4117b6b8ddd673ae6
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-11 20:21:00 +0200

    frontend:adding a mock data to pie

--- frontend/src/dashboard/Dashboard.js
+++ frontend/src/dashboard/Dashboard.js
@@ -23,27 +23,24 @@ const Dashboard = () => {
     { name: "Group E", value: 278 },
     { name: "Group F", value: 189 },
   ];
-  const renderPieChart = () => {
-    <ResponsiveContainer width="100%" height="100%">
-      <PieChart width={400} height={400}>
-        <Pie
-          dataKey="value"
-          isAnimationActive={false}
-          data={PieData}
-          cx="50%"
-          cy="50%"
-          outerRadius={80}
-          fill="#8884d8"
-          label
-        />
-
-        <Tooltip />
-      </PieChart>
-    </ResponsiveContainer>;
-  };
+  const renderPieChart = () => (
+    <PieChart width={400} height={400}>
+      <Pie
+        dataKey="value"
+        isAnimationActive={true}
+        data={PieData}
+        cx="50%"
+        cy="50%"
+        outerRadius={80}
+        fill="#884d8"
+        label
+      />
+      <Tooltip />
+    </PieChart>
+  );
   const renderBarChart = () => (
     <BarChart width={500} height={300} data={barChartData}>
-      <XAxis dataKey="name" stroke="#8884d8" />
+      <XAxis dataKey="name" stroke="#84d8" />
       <YAxis />
       <Tooltip />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
