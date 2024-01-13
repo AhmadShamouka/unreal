599b4cbcb14cb9a6f13f89b4f8dd98837a9fd77b
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-13 19:19:30 +0200

    backend:removing unique username

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->tinyInteger('admin')->default(0);
-            $table->string('username')->unique();
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('country');
