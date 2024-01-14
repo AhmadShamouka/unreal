d66c8cb0d4cb08cc2435d91a212f5627face17d1
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-14 03:14:56 +0200

    frontend:adding the name price and image in the useState and adding new variable that handle the data for image before sending it to the backend

--- frontend/src/pages/ChooseItem/ChooseItem.js
+++ frontend/src/pages/ChooseItem/ChooseItem.js
@@ -15,14 +15,24 @@ import "./styleChooseItem.css";
 
 const ChooseItem = () => {
   const navigate = useNavigate({
-    name: "",
-    price: "",
-    image: "",
+    name: "wedding",
+    price: "2992",
+    image: null,
   });
+
   const [formData, setFormData] = useState();
   const handleSubmit = async (e) => {
+    const file = e.target.files[0];
+    setFormData((prevData) => ({
+      ...prevData,
+      image: file,
+    }));
     e.preventDefault();
-    console.log(formData);
+    const formDataToSend = new FormData();
+    formDataToSend.append("name", formData.name);
+    formDataToSend.append("price", formData.price);
+    formDataToSend.append("image", formData.image);
+
     try {
       const response = await axios.post(
         "http://127.0.0.1:8000/api/add-clothes",
@@ -50,7 +60,7 @@ const ChooseItem = () => {
             navigation
             pagination={{ clickable: true }}
           >
-            <SwiperSlide onClick={handleSubmit}>
+            <SwiperSlide onClick={handleSubmit} type="file" id="imageInput">
               <img
                 src={wedding}
                 alt="Wedding"
