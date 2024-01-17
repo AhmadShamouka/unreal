8028051c46ce1b9408258cc1ee661865eecaa216
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-17 10:25:00 +0200

    frontend:adding media quary to the brand screen

--- frontend/src/pages/Brand/Brand.js
+++ frontend/src/pages/Brand/Brand.js
@@ -7,47 +7,49 @@ import HM from "../../common/images/hm.png";
 import Zara from "../../common/images/zara.png";
 const Brand = () => {
   return (
-    <div className="brand-container">
+    <div>
       <UserNavbar />
-      <div className="brand-label flex center">
-        <h1>Choose Your Brand</h1>
-      </div>
-      <hr className="hr-line" />
-      <div className="card-brand-container flex center">
-        <div className="card-brand">
-          <img
-            src={Zara}
-            alt="balloon with an emoji face"
-            className="card-img-brand"
-            loading="lazy"
-          />
-          <span className="card-footer-brand">
-            <span>Zara</span>
-            <span></span>
-          </span>
+      <div className="brand-container">
+        <div className="brand-label flex center">
+          <h1>Choose Your Brand</h1>
         </div>
-        <div className="card-brand">
-          <img
-            src={HM}
-            alt="balloon with an emoji face"
-            className="card-img-brand"
-            loading="lazy"
-          />
-          <span className="card-footer-brand">
-            <span>H & M</span>
-            <span></span>
-          </span>
-        </div>
-        <div className="card-brand">
-          <img
-            src={Gucci}
-            alt="balloon with an emoji face"
-            className="card-img-brand"
-          />
-          <span className="card-footer-brand">
-            <span>GUCCI</span>
-            <span></span>
-          </span>
+        <hr className="hr-line" />
+        <div className="card-brand-container flex center">
+          <div className="card-brand">
+            <img
+              src={Zara}
+              alt="balloon with an emoji face"
+              className="card-img-brand"
+              loading="lazy"
+            />
+            <span className="card-footer-brand">
+              <span>Zara</span>
+              <span></span>
+            </span>
+          </div>
+          <div className="card-brand">
+            <img
+              src={HM}
+              alt="balloon with an emoji face"
+              className="card-img-brand"
+              loading="lazy"
+            />
+            <span className="card-footer-brand">
+              <span>H & M</span>
+              <span></span>
+            </span>
+          </div>
+          <div className="card-brand">
+            <img
+              src={Gucci}
+              alt="balloon with an emoji face"
+              className="card-img-brand"
+            />
+            <span className="card-footer-brand">
+              <span>GUCCI</span>
+              <span></span>
+            </span>
+          </div>
         </div>
       </div>
       <Footer />
