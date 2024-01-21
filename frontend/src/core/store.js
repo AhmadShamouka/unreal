fdd55c7b6d84d11962a4c0d07fae6ed7ce6220ae
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-22 01:35:49 +0200

    frontend:creating configureStore for loginReducer

--- frontend/src/core/store.js
+++ frontend/src/core/store.js
@@ -1,4 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "../pages/Login/loginSlice/loginSlice";
 
+const store = configureStore({
+  reducer: {
+    login: loginReducer,
+  },
+});
+
 export default store;
