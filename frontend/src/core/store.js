e2c2808123ee676f4a19ff177a4da81234eb7561
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-22 03:19:38 +0200

    frontend:adding occasionSlice to the store

--- frontend/src/core/store.js
+++ frontend/src/core/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import login from "../pages/Login/loginSlice";
-
+import occasion from "../pages/Occasion/occasionSlice";
 const store = configureStore({
   reducer: {
     login: login,
+    occasion: occasion,
   },
 });
 
--- frontend/src/pages/Login/loginSlice.js
+++ frontend/src/pages/Login/loginSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { UseSelector } from "react-redux";
 const initialState = {
   username: "",
   age: "",
@@ -7,6 +7,7 @@ const initialState = {
   sex: "",
   isAuthenticated: false,
 };
+const { UrlLink } = useSelector((state) => state.occasion);
 
 const loginSlice = createSlice({
   name: "login",
