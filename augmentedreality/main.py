763295c99ef4fcd9785fc60385b5fc6428dee06a
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-24 00:47:52 +0200

    augmentedreality:fixing the scale and postion of the image

--- augmentedreality/main.py
+++ augmentedreality/main.py
@@ -23,16 +23,19 @@ def convert_to_png():
         img.save(output_path, 'PNG')
         input_img = Image.open(output_path)
         output_img = remove(input_img)
+        desired_width = 700
+        desired_height = 1000
+        img = img.resize((desired_width, desired_height))
         output_img.save(output_path)
 
-        clothes_try_on(output_path)
+        clothes_try_on()
 
-        return "Virtual try-on successful!"
+
 
     except Exception as e:
         return f"Error: {str(e)}", 500
 
-def clothes_try_on(image_path):
+def clothes_try_on():
     try:
         list_shirts = os.listdir("Resources")
         detector = PoseDetector()
@@ -51,13 +54,13 @@ def clothes_try_on(image_path):
                 img_shirt = cv2.imread(os.path.join("Resources", list_shirts[0]), cv2.IMREAD_UNCHANGED)
                 height, width, _ = img_shirt.shape
 
-                width_of_shirt = int((lm11[0] - lm12[0]) * 2)
-                height_of_shirt = int(width_of_shirt * (height / width))
+                width_of_shirt = int((lm11[0] - lm12[0]) * 3)
+                height_of_shirt = int(width_of_shirt * (height / width*1.3))
 
                 img_shirt = cv2.resize(img_shirt, (width_of_shirt, height_of_shirt))
 
-                x_offset = int(width_of_shirt * -0.5)  
-                y_offset = int(height_of_shirt * -0.1)  
+                x_offset = int(width_of_shirt * -0.35)  
+                y_offset = int(height_of_shirt * -0.17)  
 
                 top_left = (min(lm11[0], lm12[0]) + x_offset, min(lm11[1], lm12[1]) + y_offset)
 
