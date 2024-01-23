94355014b6d0bd33d85a6eaac052ae591b703652
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-23 16:14:31 +0200

    frontend:adding the api

--- frontend/src/pages/ChooseItem/ChooseItem.js
+++ frontend/src/pages/ChooseItem/ChooseItem.js
@@ -112,7 +112,7 @@ const ChooseItem = () => {
 
   const handleBuy = async () => {
     try {
-      const response = await axios.post("http://localhost:8000:");
+      const response = await axios.post("http://localhost:8000/update-trail");
     } catch (error) {
       console.log(error);
     }
