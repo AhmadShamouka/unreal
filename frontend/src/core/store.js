3c837e1f18471fb9a6d63edd381f491e3635a9cc
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-25 07:52:20 +0200

    frontend:adding chooseItem to store

--- frontend/src/core/store.js
+++ frontend/src/core/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import login from "../pages/Login/loginSlice";
 import occasion from "../pages/Occasion/occasionSlice";
+import chooseItem from "../pages/ChooseItem/ChooseItemSlice";
 const store = configureStore({
   reducer: {
     login: login,
     occasion: occasion,
+    chooseItem: chooseItem,
   },
 });
 
