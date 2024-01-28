338cff2d1e7ba4949dc38f0baad2d2937c228ad7
AhmadShamouka <shamoukaahmad@gmail.com>
2024-01-29 00:46:15 +0200

    backend:adding AdminControllerGetOccasionsTest  Feature Unit

---
+++ backend/tests/Feature/AdminControllerGetOccasionsTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+
+class AdminControllerGetOccasionsTest extends TestCase
+{
+    public function testUserLoginAndFetchUsers()
+    {
+     
+        $response = $this->postJson('/api/login', [
+            'email' =>'admin@admin.com',
+            'password' => 'password123',
+        ]);
+
+        
+        $response->assertStatus(200);
+
+        $token = $response['authorisation']['token'];
+
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->getJson('/api/admin/getoccasions');
+
+    
+        $response->assertStatus(200);
+
+      
+    }
+}
